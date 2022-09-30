3f8de23aaca40fc7c342b507075ff402e249cc6b
yxx-dev <hermitlun@gmail.com>
2022-09-30 11:11:59 -0700

    table related

--- models/index.js
+++ models/index.js
@@ -10,10 +10,16 @@ Product.belongsTo(Category, {
 })
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {through: ProductTag})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {through: ProductTag})
 
 module.exports = {
   Product,
