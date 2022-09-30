3813eea003cb9426e5afeb818a6509ad10549ead
yxx-dev <hermitlun@gmail.com>
2022-09-30 12:02:03 -0700

    working category api get route

--- models/index.js
+++ models/index.js
@@ -16,10 +16,10 @@ Category.hasMany(Product, {
 })
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {through: ProductTag})
+//Product.belongsToMany(Tag, {through: ProductTag})
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {through: ProductTag})
+//Tag.belongsToMany(Product, {through: ProductTag})
 
 module.exports = {
   Product,
--- seeds/index.js
+++ seeds/index.js
@@ -6,7 +6,7 @@ const seedProductTags = require('./product-tag-seeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-  await sequelize.sync({ force: process.env.FORCE });
+  await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
